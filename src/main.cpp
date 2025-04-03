f714b31eeea7cf8623464e757f200a6b91e0cb5d
Arthur-91140 <96424611+Arthur-91140@users.noreply.github.com>
2025-04-03 13:18:57 +0200

    Add initial structure to main.cpp with includes and main function

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <sstream>
+#include "./include/INICore.h"
+
+void main() {
+
+}
